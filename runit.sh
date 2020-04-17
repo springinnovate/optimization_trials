af62642de9a924d805b4222e77bb7b793638bd0a
Rich Sharp <richpsharp@gmail.com>
2020-04-17 11:25:36 -0700

    better startup

--- runit.sh
+++ runit.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -i
-pushd /usr/local
-git clone https://github.com/richpsharp/pygeoprocessing.git
-pushd pygeoprocessing
+pushd /usr/local/pygeoprocessing
+git pull
 git checkout feature/raster_optimization
 python setup.py install
-popd
 popd
 python optimal_land_area.py
